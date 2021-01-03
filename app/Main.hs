af734db4240e77351adc3a6f88e3885c72067f15
Jannik Theiß <jnkthss@icloud.com>
2021-01-03 19:43:46 +0100

    target args added; Main refactored

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import Control.Monad.Trans
+import Control.Monad.Except
+
 import System.Exit
 import System.Environment
 import System.Console.GetOpt
@@ -8,31 +11,79 @@ import qualified Top
 
 
 data Options = Options {
-    _debug :: Bool
+    _debug :: Bool,
+    _target :: String,
+    _triple :: Maybe String,
+    _cpu :: Maybe String
   } deriving Show
 
 defaultOptions :: Options
 defaultOptions = Options {
-    _debug = Top._debug Top.defaultOptions
+    _debug = Top._debug Top.defaultOptions,
+    _target = "native",
+    _triple = Nothing,
+    _cpu = Nothing
   }
 
 optionDescriptions :: [OptDescr (Options -> Options)]
 optionDescriptions = [
-    Option ['d'] ["debug"] (NoArg (\opts -> opts { _debug = True })) "debug mode"
+    Option ['d'] ["debug"] (NoArg (\opts -> opts { _debug = True })) "debug mode",
+    Option [] ["target"] (ReqArg (\target opts -> opts { _target = target }) "<arg>") "",
+    Option [] ["triple"] (ReqArg (\triple opts -> opts { _triple = Just triple }) "<arg>") "",
+    Option [] ["cpu"] (ReqArg (\cpu opts -> opts { _cpu = Just cpu }) "<arg>") ""
   ]
 
 main :: IO ()
 main = do
-  args <- getArgs
+
+  result <- runExceptT main'
+
+  case result of
+    Left err -> do
+      putStrLn err
+      exitFailure
+
+    Right () ->
+      exitSuccess
+
+
+main' :: (MonadError String m, MonadIO m) => m ()
+main' = do
+
+  args <- liftIO getArgs
+
   (options, fileNames) <- case getOpt Permute optionDescriptions args of
     (o, n, [])   -> return (foldl (flip id) defaultOptions o, n)
-    (_, _, errs) -> ioError (userError (concat errs ++ usageInfo "Usage: ..." optionDescriptions))
+    (_, _, errs) -> throwError (concat errs ++ usageInfo "Usage: ..." optionDescriptions)
+
+  target <- case _target options of
+    "native" -> return Top.NativeTarget
+
+    "embedded" -> do
+      triple <- maybeToError (_triple options) "triple must be specified when using target embedded"
+      cpu <- maybeToError (_cpu options) "cpu must be specified when using target embedded"
+      return $ Top.EmbeddedTarget triple cpu
+
+    "arduino" -> do
+      triple <- maybeToError (_triple options) "triple must be specified when using target arduino"
+      cpu <- maybeToError (_cpu options) "cpu must be specified when using target arduino"
+      return $ Top.ArduinoTarget triple cpu
+
+    target' -> throwError $ "invalid target: " ++ target'
+
   let processOptions = Top.defaultOptions {
-      Top._debug = _debug options
+      Top._debug = _debug options,
+      Top._target = target
     }
+
   case fileNames of
-    []             -> Top.repl processOptions
+    [] -> Top.repl processOptions
+
     (fileName : _) -> do
       success <- Top.processFile processOptions fileName
-      if (success) then exitSuccess else exitFailure
+      if (success) then return () else throwError "compilation failed"
 
+
+maybeToError :: MonadError e m => Maybe a -> e -> m a
+maybeToError (Just x) _ = return x
+maybeToError Nothing err = throwError err
