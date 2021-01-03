a1dc1ed58fd8b34fa3565d018c2f568e7234263c
Jannik Theiß <jnkthss@icloud.com>
2021-01-03 15:58:53 +0100

    refactored exmples and tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,12 +2,9 @@ module Main where
 
 import Prelude
 
-import Control.Monad
 import Control.Exception
 
-import System.Exit
 import System.Directory
-import System.Process
 import System.IO.Error
 
 import Test.Hspec
@@ -16,58 +13,28 @@ import Top
 
 
 main :: IO ()
-main = hspec $ beforeAll (setCurrentDirectory "test") $ do
+main = hspec $ do
 
   describe "Top" $ do
+    specifyCompileExample "native_example" NativeTarget
+    specifyCompileExample "LPC810_blinky" (EmbeddedTarget "thumbv6m-none--eabi" "cortex-m0")
+    specifyCompileExample "arduino_blinky" (ArduinoTarget "avr-atmel-none" "atmega328p")
 
-    specify "compile and run example.mll" $ do
 
-      let name = "example"
+specifyCompileExample :: String -> Target -> Spec
+specifyCompileExample name target = specify ("compile " ++ name) $ withCurrentDirectory ("examples/" ++ name) $ do
 
-      let files = fmap ("build/" ++) [name ++ ".ll", name ++ ".s", name ++ ".o", "builtins.o", name]
-      mapM_ removeFileIfExists files
+  let files = fmap ("build/" ++) [name ++ ".ll", name ++ ".s", name ++ ".o"]
+  mapM_ removeFileIfExists files
 
-      let options = Top.defaultOptions
+  let options = Top.defaultOptions {
+      _target = target
+    }
 
-      success <- processFile options (name ++ ".mll")
+  success <- processFile options (name ++ ".mll")
 
-      shouldBe success True
+  shouldBe success True
 
-      (exitCode, stdOutput, _) <- readProcessWithExitCode ("./build/" ++ name) [] ""
-      putStr stdOutput
-      when (stdOutput == "" || last stdOutput /= '\n') $ putStr "%\n"
-
-      shouldBe exitCode ExitSuccess
-
-    specify "compile LPC810_blinky.mll" $ do
-
-      let name = "LPC810_blinky"
-
-      let files = fmap ("build/" ++) [name ++ ".ll", name ++ ".s", name ++ ".o"]
-      mapM_ removeFileIfExists files
-
-      let options = Top.defaultOptions {
-          _target = EmbeddedTarget "thumbv6m-none--eabi" "cortex-m0"
-        }
-
-      success <- processFile options (name ++ ".mll")
-
-      shouldBe success True
-
-    specify "compile arduino_blinky.mll" $ do
-
-      let name = "arduino_blinky"
-
-      let files = fmap ("build/" ++) [name ++ ".ll", name ++ ".s", name ++ ".o"]
-      mapM_ removeFileIfExists files
-
-      let options = Top.defaultOptions {
-          _target = ArduinoTarget "avr-atmel-none" "atmega328p"
-        }
-
-      success <- processFile options (name ++ ".mll")
-
-      shouldBe success True
 
 removeFileIfExists :: FilePath -> IO ()
 removeFileIfExists fileName = removeFile fileName `catch` handleExists
